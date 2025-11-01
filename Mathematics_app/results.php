a534668ec777d4cf9d7de25ced88aeb580774646
Enma0101 <156552875+Enma0101@users.noreply.github.com>
2025-11-01 05:19:15 +0000

    prueba

--- Mathematics_app/results.php
+++ Mathematics_app/results.php
@@ -1,5 +1,4 @@
-<?php 
-
-# Historial de resultados
-
+<?php
+echo "<h1 style='text-align:center; margin-top:50px; font-family:Arial;'>👋 Hello World from CyberPanel + GitHub!</h1>";
+echo "<p style='text-align:center;'>Deployed on: " . date('Y-m-d H:i:s') . "</p>";
 ?>
