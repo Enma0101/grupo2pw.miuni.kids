8fd13c9189d7481a0dc8bf931098c2868f35d9b7
Enma0101 <156552875+Enma0101@users.noreply.github.com>
2025-11-01 05:20:24 +0000

    prueba2

--- Mathematics_app/index.php
+++ Mathematics_app/index.php
@@ -2,6 +2,8 @@
 # Página de login
 
 
+echo "<h1 style='text-align:center; margin-top:50px; font-family:Arial;'>👋 Hello World from CyberPanel + GitHub!</h1>";
+echo "<p style='text-align:center;'>Deployed on: " . date('Y-m-d H:i:s') . "</p>";
 
 
 ?>
--- Mathematics_app/results.php
+++ Mathematics_app/results.php
@@ -1,4 +1,5 @@
-<?php
-echo "<h1 style='text-align:center; margin-top:50px; font-family:Arial;'>👋 Hello World from CyberPanel + GitHub!</h1>";
-echo "<p style='text-align:center;'>Deployed on: " . date('Y-m-d H:i:s') . "</p>";
+<?php 
+
+# Historial de resultados
+
 ?>
